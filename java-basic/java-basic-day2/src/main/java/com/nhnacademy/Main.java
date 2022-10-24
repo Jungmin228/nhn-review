81cb9e2721d35c1eea262fa4889a048edb2e0529
moomink <moomink228@gmail.com>
2022-10-24 19:12:19 +0900

    20221024 버블정렬

--- java-basic/java-basic-day2/src/main/java/com/nhnacademy/Main.java
+++ java-basic/java-basic-day2/src/main/java/com/nhnacademy/Main.java
@@ -1,7 +1,15 @@
 package com.nhnacademy;
 
+import com.nhnacademy.example.BubbleSort;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // review 버블정렬
+//        BubbleSort bubbleSort = new BubbleSort();
+//        bubbleSort.printNumbers();
+//        bubbleSort.initNumbers();
+//        bubbleSort.printNumbers();
+//        bubbleSort.sort();
+//        bubbleSort.printNumbers();
     }
 }
---
+++ java-basic/java-basic-day2/src/main/java/com/nhnacademy/example/BubbleSort.java
@@ -0,0 +1,34 @@
+package com.nhnacademy.example;
+
+import java.util.Random;
+
+public class BubbleSort {
+    int[] numbers = new int[10];
+
+    public void initNumbers() {
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(20) + 1;
+        }
+    }
+
+    public void sort() {
+        for (int i = 1; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length - i; j++) {
+                if (numbers[j] > numbers[j+1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void printNumbers() {
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+    }
+}
