3a97054efca2146af452bfba48382fa50a41afae
moomink <moomink228@gmail.com>
2022-10-24 21:24:57 +0900

    20221024 길이 100 배열에서 10 이하 숫자 출력

--- java-basic/java-basic-day2/src/main/java/com/nhnacademy/Main.java
+++ java-basic/java-basic-day2/src/main/java/com/nhnacademy/Main.java
@@ -1,6 +1,8 @@
 package com.nhnacademy;
 
 import com.nhnacademy.example.BubbleSort;
+import com.nhnacademy.example.LessThan10;
+import com.nhnacademy.task.TicTacToe;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +13,12 @@ public class Main {
 //        bubbleSort.printNumbers();
 //        bubbleSort.sort();
 //        bubbleSort.printNumbers();
+
+        // review 길이 100 배열에서 10 이하 숫자 출력
+        LessThan10 lessThan10 = new LessThan10();
+        lessThan10.initNumbers();
+        lessThan10.printNumbers(false);
+        lessThan10.printNumbers(true);
+        
     }
 }
---
+++ java-basic/java-basic-day2/src/main/java/com/nhnacademy/example/LessThan10.java
@@ -0,0 +1,26 @@
+package com.nhnacademy.example;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static java.util.Arrays.stream;
+
+public class LessThan10 {
+    int[] numbers = new int[100];
+
+    public void initNumbers() {
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100) + 1;
+        }
+    }
+
+    public void printNumbers(boolean less) {
+        if (less) {
+            System.out.println(Arrays.toString(stream(numbers).filter(i -> i <= 10).toArray()));
+        } else{
+            System.out.println(Arrays.toString(numbers));
+        }
+    }
+}
